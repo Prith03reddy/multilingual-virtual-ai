97cb57ea2acbfc9487989fe0a71fc14af0f90cb9
prithvi kumar <prithvikumarreddy2@gmail.com>
2025-05-12 18:41:32 +0530

    third commit

--- setup.py
+++ setup.py
@@ -2,12 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name="genai AI project",  # Fixed name formatting
-    version="0.0.0",
+    version="0.0.1",
     author="Amrita",
     author_email="prithvikumarreddy2@gmail.com", 
     packages=find_packages()
-    # install_requires=[],  # Add dependencies here if needed
-    # description="A Generative AI Project",
-    # license="MIT",
-    # long_description="This is a generative AI project created for experimentation and development."
 )
